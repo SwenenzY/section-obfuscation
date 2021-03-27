f7afedec6e9de2a279ccadf83990cba34a6a1ae3
Yiğit Kaan Bayat <ykaanbayat@gmail.com>
2021-03-27 11:57:44 +0300

    Section Header info

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,36 @@ int main(int argc,char* argv[])
 
                 // READ DOS HEADER 
                 PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER)fileData;
-                std::cout << "[*] Dos Header Info;" << std::endl;
+                std::cout << "\n[*] Dos Header Info;" << std::endl;
                 printf("[+] 0x%x\t : Magic Number(e_magic)\n", dosHeader->e_magic);
                 printf("[+] 0x%x\t : File address of new exe header(e_lfanew)\n", dosHeader->e_lfanew);
 
                 // READ IMAGE_NT_HEADERS
-                std::cout << "[*] Image NT Headers Info;" << std::endl;
                 PIMAGE_NT_HEADERS imageNTHeader = (PIMAGE_NT_HEADERS)((DWORD)fileData + dosHeader->e_lfanew);
 
+                std::cout << "\n[*] Image NT Headers Info;" << std::endl;
                 printf("[+] 0x%x\t : Signature\n", imageNTHeader->Signature);
                 printf("[+] 0x%x\t : Time Date Stamp\n", imageNTHeader->FileHeader.TimeDateStamp);
                 printf("[+] 0x%x\t : Size Of Optional Header\n", imageNTHeader->FileHeader.SizeOfOptionalHeader);
+                printf("[+] %x\t\t : Number Of Sections\n", imageNTHeader->FileHeader.NumberOfSections);
+
+
+
+                std::cout << "\n[*] Optimal Header Info;" << std::endl;
+                // start point .text or .vmp
+                printf("[+] 0x%x\t : AddressOfEntryPoint\n", imageNTHeader->OptionalHeader.AddressOfEntryPoint);
+
+                // we need it
+                std::cout << "\n[*] Section Header Info;" << std::endl;
+                DWORD sectionLocation = (DWORD)imageNTHeader + sizeof(DWORD) + (DWORD)(sizeof(IMAGE_FILE_HEADER)) + (DWORD)imageNTHeader->FileHeader.SizeOfOptionalHeader;
+
+                auto sectionHeader = (PIMAGE_SECTION_HEADER)sectionLocation;
+
+                for (size_t i = 0; i < imageNTHeader->FileHeader.NumberOfSections; i++, ++sectionHeader)
+                {
+                    printf("[+] %s\t : Section Text\n", sectionHeader->Name);
+                }
+
             }
             else {
                 printf("[-] Failed to open specified file. (%d)\n", file);
