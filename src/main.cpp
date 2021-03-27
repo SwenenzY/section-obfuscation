96f6b1c57f13e573782fff63e65b3e54d411443b
Yiğit Kaan Bayat <ykaanbayat@gmail.com>
2021-03-27 11:16:34 +0300

    save 11:16 AM

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <filesystem>
 
-int main()
+
+int main(int argc,char* argv[])
 {
-    std::cout << "Hello World!\n";
+    // have args
+    if (argv[1]) {
+        // file exits
+        if (std::filesystem::exists(argv[1])) {
+            std::cout << "[!] Working file : " << argv[1] << std::endl; // print
+
+        }
+    }
+    else {
+        std::cout << "[-] File is not specified, please use this project.exe any.exe/.dll" << std::endl;
+    }
+
+    system("pause");
 }
